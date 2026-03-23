9b0702653de50415e2a58d8e010e52d3ff3d3648
JustineDevs <TraderGOfficial@gmail.com>
2026-03-23 21:48:09 +0800

    src/app/page.tsx – Updated "use client"; to 'use client';

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,39 @@
-"use client";
-import React from 'react'
-import PerksPage from './components/perks-page';
+'use client';
+
+import React from 'react';
 import HeroPage from './components/hero-page';
+import HowItWorks from './components/how-it-works';
 import ValueProposition from './components/value-proposition';
+import PerksPage from './components/perks-page';
+import Section from './components/section';
+import SectionNav from './components/section-nav';
+import HeroBackground from './components/hero-background';
 
-const page = () => {
+export default function Page() {
   return (
-    <div className='bg-black'>
-      <HeroPage />
-      <ValueProposition />
-      <PerksPage />
-    </div>
-  )
-}
+    <div className="relative min-h-screen bg-[#050507]">
+      <SectionNav />
 
-export default page
+      <Section id="hero" fullHeight className="overflow-hidden">
+        <HeroBackground />
+        <div className="relative z-[5]">
+          <HeroPage />
+        </div>
+      </Section>
+
+      <Section id="how-it-works">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6">
+          <HowItWorks />
+        </div>
+      </Section>
+
+      <Section id="about">
+        <ValueProposition />
+      </Section>
+
+      <Section id="perks">
+        <PerksPage />
+      </Section>
+    </div>
+  );
+}
